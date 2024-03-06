f1f4dc6c131d73f056970f05ff33f79d142cd6e8
Jojobster <162330326+Jojobster@users.noreply.github.com>
2024-03-06 09:17:16 +0200

    Update Program.cs

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -5,10 +5,8 @@ class Program
 {
     static void Main()
     {
-        // Приклад послідовності цілих чисел
         int[] sequence = { 43, 47, 87 };
 
-        // Використовуємо метод Aggregate для знаходження твору останніх цифр
         double productOfLastDigits = CalculateProductOfLastDigits(sequence);
 
         Console.WriteLine($"Твір останніх цифр: {productOfLastDigits}");
@@ -16,13 +14,10 @@ class Program
 
     static double CalculateProductOfLastDigits(int[] sequence)
     {
-        // Використовуємо метод Aggregate для обчислення добутку останніх цифр
         return sequence.Aggregate(1.0, (product, number) =>
         {
-            // Отримуємо останню цифру числа за допомогою % 10
             int lastDigit = Math.Abs(number % 10);
 
-            // Множимо поточний добуток на останню цифру
             return product * lastDigit;
         });
     }
